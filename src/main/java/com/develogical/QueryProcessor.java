3fb8ac28d2a608b72844ffa6b86aa54e64965293
juliawgraham <juliagra@andrew.cmu.edu>
2022-09-22 08:45:04 -0400

    changed new query from test to hello (b/c tests)

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,7 +8,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if (query.toLowerCase().contains("test")) {
+        if (query.toLowerCase().contains("hello")) {
             return "Testing this app";
         }
         return "";
